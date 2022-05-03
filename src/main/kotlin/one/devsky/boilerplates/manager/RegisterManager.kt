e38afaa6911559a0f04925761599eb5011d10acd
CoasterFreakDE <mleaked@gmail.com>
2022-05-03 13:30:17 +0200

    🔥Added full command data support

--- src/main/kotlin/one/devsky/boilerplates/manager/RegisterManager.kt
+++ src/main/kotlin/one/devsky/boilerplates/manager/RegisterManager.kt
@@ -5,12 +5,17 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.hooks.EventListener
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.build.Commands
 import one.devsky.boilerplates.annotations.SlashCommand
+import one.devsky.boilerplates.interfaces.HasSubcommandGroups
+import one.devsky.boilerplates.interfaces.HasOptions
 import org.reflections8.Reflections
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
 object RegisterManager {
+
+    private var loadedClasses = mapOf<String, Any>()
 
     @OptIn(ExperimentalTime::class)
     fun JDABuilder.registerAll() : JDABuilder {
@@ -25,6 +30,7 @@ object RegisterManager {
                 constructor.trySetAccessible()
 
                 val listener = constructor.newInstance()
+                loadedClasses += clazz.simpleName to listener
                 addEventListeners(listener)
                 getLogger(RegisterManager::class).info("Registered listener: ${listener.javaClass.simpleName}")
             }
@@ -42,14 +48,34 @@ object RegisterManager {
         val commandsTime = measureTime {
             for (clazz in reflections.getTypesAnnotatedWith(SlashCommand::class.java)) {
                 val annotation = clazz.getAnnotation(SlashCommand::class.java)
+                val data = Commands.slash(annotation.name, annotation.description)
+
+                if (clazz.simpleName !in loadedClasses) {
+                    val constructor = clazz.getDeclaredConstructor()
+                    constructor.trySetAccessible()
+
+                    val command = constructor.newInstance()
+                    loadedClasses += clazz.simpleName to command
+                    getLogger(RegisterManager::class).info("Registered command class: ${command.javaClass.simpleName}")
+                }
+
+                val command = loadedClasses[clazz.simpleName]
+
+                if (command is HasOptions) {
+                    data.addOptions(command.getOptions())
+                }
+
+                if (command is HasSubcommandGroups) {
+                    data.addSubcommandGroups(command.getChoices())
+                }
 
                 if(annotation.globalCommand) {
-                    upsertCommand(annotation.name, annotation.description).queue()
+                    upsertCommand(data).queue()
                     getLogger(RegisterManager::class).info("Registered global command: ${annotation.name}")
                 } else {
                     for (guildID in annotation.guilds) {
                         getGuildById(guildID)?.let { guild ->
-                            guild.upsertCommand(annotation.name, annotation.description).queue()
+                            guild.upsertCommand(data).queue()
                             getLogger(RegisterManager::class).info("Registered command: ${annotation.name} in guild: ${guild.name}")
                         }
                     }
